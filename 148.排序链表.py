0ae696a166b67ae72855d6701fa9ea0aee020e38
weshao <shihao.wei@doneenterpsies.fi>
2021-02-08 15:59:59 +0100

    review of 29, 148

--- 148.排序链表.py
+++ 148.排序链表.py
@@ -12,38 +12,44 @@ class ListNode:
         self.val = x
         self.next = None
 
-# class Solution:
-#     def sortList(self, head: ListNode) -> ListNode:
-#         if head is None or head.next is None:
-#             return head
-#         slow, fast = head, head
-#         while fast.next and fast.next.next:
-#             slow = slow.next
-#             fast = fast.next.next
+""" class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        def divid_and_merge(head):
+            if not head.next:
+                return head
+            
+            slow, fast = head, head.next
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            
+            temp = slow.next
+            slow.next = None
+            left = divid_and_merge(head)
+            right = divid_and_merge(temp)
 
-#         left = head
-#         right = slow.next
-#         slow.next = None
-#         sortedLeft = self.sortList(left)
-#         sortedRight = self.sortList(right)
-#         dummyHead = ListNode(0)
-#         dummyHead2 = dummyHead
-#         while sortedLeft and sortedRight:
-#             if sortedLeft.val < sortedRight.val:
-#                 dummyHead.next = sortedLeft
-#                 sortedLeft = sortedLeft.next
-#             else:
-#                 dummyHead.next = sortedRight
-#                 sortedRight = sortedRight.next
-#             dummyHead = dummyHead.next
-#         dummyHead.next = sortedRight if sortedRight else sortedLeft
-#         return dummyHead2.next
+            dumpHead = ListNode(0)
+            rdumpHead = dumpHead
+            while left and right:
+                if left.val < right.val:
+                    dumpHead.next = left
+                    left = left.next
+                else:
+                    dumpHead.next = right
+                    right = right.next
+                dumpHead = dumpHead.next
+            dumpHead.next = left if left else right
+
+            return rdumpHead.next
+        
+        return divid_and_merge(head) if head else None """
 
 class Solution:
     def sortList(self, head: ListNode) -> ListNode:
         h, length, intv = head, 0, 1
         while h:
-            h, length = h.next, length + 1
+            length += 1
+            h = h.next
         res = ListNode(0)
         res.next = head
         while intv < length:
@@ -51,14 +57,39 @@ class Solution:
             while h:
                 h1, i = h, intv
                 while i and h:
-                    h, i = 
-
+                    h, i = h.next, i- 1
+                if i:
+                    break
+                h2, i = h, intv
+                while i and h:
+                    h, i  = h.next, i - 1
+                c1, c2 = intv, intv - i
+                while c1 and c2:
+                    if h1.val < h2.val:
+                        pre.next = h1
+                        h1 = h1.next
+                        c1 = c1 - 1
+                    else:
+                        pre.next = h2
+                        h2 = h2.next
+                        c2 = c2 - 1
+                    pre = pre.next
+                pre.next = h1 if c1 else h2
+                while c1 > 0 or c2 > 0:
+                    pre = pre.next
+                    c1 = c1 - 1
+                    c2 = c2 - 1
+                pre.next = h
+            intv *= 2
+        return res.next
 # @lc code=end
-node = ListNode(-1)
-node.next = ListNode(5)
-node.next.next = ListNode(3)
-node.next.next.next = ListNode(4)
-node.next.next.next.next = ListNode(0)
+node = ListNode(4)
+node.next = ListNode(3)
+node.next.next = ListNode(2)
+node.next.next.next = ListNode(6)
+node.next.next.next.next = ListNode(5)
+node.next.next.next.next.next = ListNode(7)
+node.next.next.next.next.next.next = ListNode(1)
 so = Solution()
 ans = so.sortList(node)
 print(ans)
--- 29.两数相除.py
+++ 29.两数相除.py
@@ -7,5 +7,36 @@
 # @lc code=start
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-# @lc code=end
+        neg = False
+        if dividend == 0:
+            return 0
+        if (dividend < 0 and divisor > 0) or (dividend > 0 and divisor < 0):
+            neg = True
+        dividend, divisor = abs(dividend), abs(divisor)
+        
+        res = 1
+        result = 0
+        ori_divisor = divisor
+        while divisor <= dividend:
+            result += res
+            dividend -= divisor
+            divisor = divisor << 1
+            res = res << 1
 
+        while dividend >= ori_divisor:
+            divisor = divisor >> 1
+            res = res >> 1
+            if dividend >= divisor:
+                result += res
+                dividend -= divisor
+        if neg and result > 2 ** 31 or (not neg and result > (2 ** 31 - 1)):
+            return 2 ** 31 - 1
+        return result if not neg else -result
+
+
+
+
+# @lc code=end
+so = Solution()
+ans = so.divide(-2147483648, -1)
+print(ans)
